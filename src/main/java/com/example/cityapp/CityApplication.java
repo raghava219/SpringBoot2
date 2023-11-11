2ae770b24211a16a3e07633739708dc9182f659e
raghava219 <raghava219@gmail.com>
2023-11-11 08:00:00 +0530

    Modified depedencies.

--- src/main/java/com/example/cityapp/CityApplication.java
+++ src/main/java/com/example/cityapp/CityApplication.java
@@ -4,7 +4,9 @@ import io.micrometer.core.instrument.MeterRegistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
